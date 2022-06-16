3cebd0e8ed98997c3f0883e23d8e6f293156e633
Han Tran <imhantran@gmail.com>
2022-06-16 13:48:57 +0700

    remove info

--- config/dev.js
+++ config/dev.js
@@ -3,9 +3,9 @@ module.exports = {
     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    "mongodb+srv://hantran:hantran1510@cluster0.rsvwc.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://<user>:<password>@cluster0.rsvwc.mongodb.net/?retryWrites=true&w=majority",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379",
-  accessKeyId: 'AKIAZN7TSYKUVZTL6E7M',
-  secretAccessKey: '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'
+  accessKeyId: '',
+  secretAccessKey: ''
 };
